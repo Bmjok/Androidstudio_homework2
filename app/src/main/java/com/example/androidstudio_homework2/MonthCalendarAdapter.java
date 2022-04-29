6dc4aca11145b75e809d43a95009f2c91ef4c264
Sebbange <sebbang99@gmail.com>
2022-04-30 01:56:12 +0900

    Viewpager2 삭제
    다시 추가 예정...

--- app/src/main/java/com/example/androidstudio_homework2/MonthCalendarAdapter.java
+++ app/src/main/java/com/example/androidstudio_homework2/MonthCalendarAdapter.java
@@ -1,31 +1,54 @@
 package com.example.androidstudio_homework2;
 
-import androidx.annotation.NonNull;
-import androidx.fragment.app.Fragment;
-import androidx.fragment.app.FragmentActivity;
-import androidx.viewpager2.adapter.FragmentStateAdapter;
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.GridView;
 
+import java.util.ArrayList;
 import java.util.Calendar;
 
-public class MonthCalendarAdapter extends FragmentStateAdapter {
+public class MonthCalendarAdapter extends BaseAdapter {
 
-    public MonthCalendarAdapter(@NonNull MonthCalendarFragment fragmentActivity) {
-        super(fragmentActivity);
-    }
+    private Context Context;
+    private int Resource;
+    private ArrayList<MonthCalendarAdapter> Items = new ArrayList<MonthCalendarAdapter>();
+    Calendar cal = Calendar.getInstance();
 
-    //Each Page
-    @NonNull
-    @Override
-    public Fragment createFragment(int position) {
-        Calendar cal = Calendar.getInstance();
-        int year = cal.get(Calendar.YEAR);
-        int month = position;
-        return MonthCalendarFragment.newInstance(year, month); //Fragment Instance
+    public MonthCalendarAdapter (Context context, int resource, ArrayList<MonthCalendarAdapter> items) {
+        Context = context;
+        Resource = resource;
+        Items = items;
     }
 
     @Override
-    public int getItemCount() {
-        return 12; //수정해야됨 **************************
+    public int getCount() {
+        return Items.size();
+    }
+
+    @Override
+    public Object getItem(int position) {
+        return Items.get(position);
+    }
+
+    @Override
+    public long getItemId(int position) {
+        return position;
+    }
+
+    @Override
+    public View getView(int i, View view, ViewGroup viewGroup) {
+        if (view == null) {
+            LayoutInflater inflater = (LayoutInflater)
+                    Context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+            view = inflater.inflate(Resource, viewGroup,false);
+        }
+
+        GridView day = (GridView) view.findViewById(R.id.gridview);
+
+        return view;
     }
 
 }
--- app/src/main/java/com/example/androidstudio_homework2/MonthCalendarFragment.java
+++ app/src/main/java/com/example/androidstudio_homework2/MonthCalendarFragment.java
@@ -17,25 +17,28 @@ import java.util.ArrayList;
 import java.util.Calendar;
 
 public class MonthCalendarFragment extends Fragment {
-    //교재 TitlesFragment 같은 존재
     //MonthCalendarFragment <-> MonthCalendarAdapter
+    ArrayList<MonthCalendarAdapter> items = new ArrayList<MonthCalendarAdapter>();
     Calendar cal = Calendar.getInstance();
 
     private static final String ARG_PARAM1 = "year";
     private static final String ARG_PARAM2 = "month";
+    private static final String ARG_PARAM3 = "day";
 
     private int year;
     private int month;
+    public int day;
 
     public MonthCalendarFragment() {
 
     }
 
-    public static MonthCalendarFragment newInstance(int year, int month) {
+    public static MonthCalendarFragment newInstance(int year, int month, int day) {
         MonthCalendarFragment fragment = new MonthCalendarFragment();
         Bundle args = new Bundle();
         args.putInt(ARG_PARAM1, year);
         args.putInt(ARG_PARAM2, month);
+        args.putInt(ARG_PARAM3, day);
         fragment.setArguments(args);
         return fragment;
     }
@@ -56,6 +59,12 @@ public class MonthCalendarFragment extends Fragment {
         View rootView = inflater.inflate(R.layout.fragment_monthcalendar,
                 container, false);
         GridView gridView = rootView.findViewById(R.id.gridview);
+        gridView.setAdapter(
+                new ArrayAdapter<MonthCalendarAdapter>(
+                        getActivity(),
+                        android.R.layout.simple_list_item_activated_1,
+                        items)); //데이터 추가 필요함 ******************
+
         gridView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> adapterView, View view, int position, long id) {
@@ -65,6 +74,7 @@ public class MonthCalendarFragment extends Fragment {
                     ((OnTitleSelectedListener)activity).onTitleSelected(position);
             }
         });
+
         return rootView;
     }
 
