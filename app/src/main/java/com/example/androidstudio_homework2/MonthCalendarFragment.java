c1e77e7d18a858c1a51794ee1aaf8d6e8bc25629
Sebbange <sebbang99@gmail.com>
2022-04-30 04:06:58 +0900

    액션바 타이틀 수정중2

--- app/src/main/java/com/example/androidstudio_homework2/MonthCalendarFragment.java
+++ app/src/main/java/com/example/androidstudio_homework2/MonthCalendarFragment.java
@@ -1,6 +1,7 @@
 package com.example.androidstudio_homework2;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.appcompat.app.ActionBar;
@@ -12,6 +13,7 @@ import android.view.ViewGroup;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.GridView;
+import android.widget.Toast;
 
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
@@ -19,14 +21,15 @@ import java.util.Calendar;
 
 public class MonthCalendarFragment extends Fragment {
     //MonthCalendarFragment <-> MonthCalendarAdapter
-    ArrayList<String> days = new ArrayList<String>();
-    Calendar cal;
+    ArrayList<String> days = new ArrayList<>();
+    Calendar cal = Calendar.getInstance();
 
     private static final String ARG_PARAM1 = "year";
     private static final String ARG_PARAM2 = "month";
 
-    private int year;
-    private int month;
+    int year;
+    int month;
+    static int day = 1;
 
     public MonthCalendarFragment() {
 
@@ -35,7 +38,6 @@ public class MonthCalendarFragment extends Fragment {
     public static MonthCalendarFragment newInstance(int year, int month) {
         //매개변수로 년도와 월을 받음
         MonthCalendarFragment fragment = new MonthCalendarFragment();
-        Calendar.getInstance().set(year, month, 1);
         Bundle args = new Bundle();
         args.putInt(ARG_PARAM1, year);
         args.putInt(ARG_PARAM2, month);
@@ -47,10 +49,17 @@ public class MonthCalendarFragment extends Fragment {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
+        /*
+        if (getArguments() != null) {
+            year = getArguments().getInt(ARG_PARAM1);
+            month = getArguments().getInt(ARG_PARAM2);
+        }
+        */
+
         //액션바 타이틀 변경(setTitle()메소드): https://onlyfor-me-blog.tistory.com/196
-        ((MainActivity) getActivity()).setTitle(year+"년 "+month+"월");
+        ActionBar ab = ((MainActivity) getActivity()).getSupportActionBar();
+        ab.setTitle(year+"년 "+(month+1)+"월");
         //******** 이대로 두면 안되고, 이동하면 날짜가 바뀌게 수정해야함
-        //날짜가 현재는 0년 0월로 뜸
 
     }
 
@@ -58,11 +67,25 @@ public class MonthCalendarFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
 
+        year = cal.get(Calendar.YEAR);
+        month = cal.get(Calendar.MONTH);
+
         View rootView = inflater.inflate(R.layout.fragment_monthcalendar,
                 container, false);
         GridView gridView = rootView.findViewById(R.id.gridview);
+
+        int startday = cal.get(Calendar.DAY_OF_WEEK);
+        if(startday != 1) {
+            for(int i=0; i<startday-1; i++) {
+                days.add("");
+            }
+        } // 매월 1일이 요일과 일치하지 않으면 공백 출력
+        for (int i = 0; i < cal.getActualMaximum(Calendar.DAY_OF_MONTH); i++) {
+            days.add("" + (i + 1));
+        } // 매월 1일이 요일과 일치하면 (ex: 22년 4월 1일은 금요일) 그때부터 해당 말일(4월은 30일)까지 날짜 출력
+
         gridView.setAdapter(
-                new ArrayAdapter<String>(
+                new ArrayAdapter<>(
                         getActivity(),
                         android.R.layout.simple_list_item_activated_1,
                         days)); //데이터 추가 필요함 ******************
@@ -73,7 +96,9 @@ public class MonthCalendarFragment extends Fragment {
                 Activity activity = getActivity();
 
                 if (activity instanceof OnTitleSelectedListener)
-                    ((OnTitleSelectedListener)activity).onTitleSelected(position);
+                    ((OnTitleSelectedListener)activity).onTitleSelected((month+1));
+                Toast.makeText(getActivity(), month,Toast.LENGTH_SHORT).show();
+                //일 부분 수정해야함 **********************
             }
         });
 
