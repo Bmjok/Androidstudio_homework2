d2a071ff28135aa64b65deb81db92ac583d787dc
Sebbange <sebbang99@gmail.com>
2022-04-29 23:07:50 +0900

    앱바 추가 (수정필요)

--- app/src/main/java/com/example/androidstudio_homework2/MainActivity.java
+++ app/src/main/java/com/example/androidstudio_homework2/MainActivity.java
@@ -7,6 +7,9 @@ import androidx.fragment.app.FragmentTransaction;
 import android.content.Intent;
 import android.content.res.Configuration;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.widget.Toast;
 
 import java.util.ArrayList;
@@ -20,17 +23,43 @@ public class MainActivity extends AppCompatActivity implements MonthCalendarFrag
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        FragmentManager fragmentManager = getSupportFragmentManager();
-        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
-        fragmentTransaction.add(R.id.fragment_container, new MonthFragment());
-        fragmentTransaction.commit();
-
         Calendar cal = Calendar.getInstance();
 
     }
 
     public void onTitleSelected(int i) {
         Toast.makeText(getApplicationContext(), "position"+i, Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.main_menu, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.action_month:
+                //액션바 월 선택 -> 월 달력 출력
+                FragmentManager fragmentManager = getSupportFragmentManager();
+                FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+                fragmentTransaction.replace(R.id.fragment_container, new MonthFragment());
+                fragmentTransaction.commit();
+                //MonthFragment 불러오기
+                return true;
+            case R.id.action_week:
+                //액션바 주 선택 -> 주 달력 출력
+                fragmentManager = getSupportFragmentManager();
+                fragmentTransaction = fragmentManager.beginTransaction();
+                fragmentTransaction.replace(R.id.fragment_container, new WeekFragment()); //이름대강정함
+                fragmentTransaction.commit();
+                //WeekFragment 불러오기
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
     }
 
 }
