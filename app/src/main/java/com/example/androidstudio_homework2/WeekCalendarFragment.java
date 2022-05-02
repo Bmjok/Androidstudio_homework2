b9fb5207856c6b740c6eedcc819d737340451ae8
Sebbange <sebbang99@gmail.com>
2022-05-03 00:20:30 +0900

    WeekCalendarFragment 수정

--- app/src/main/java/com/example/androidstudio_homework2/WeekCalendarFragment.java
+++ app/src/main/java/com/example/androidstudio_homework2/WeekCalendarFragment.java
@@ -6,8 +6,12 @@ import androidx.appcompat.app.ActionBar;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
+import android.view.Surface;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.GridView;
+import android.widget.Toast;
 
 import java.util.ArrayList;
 import java.util.Calendar;
@@ -68,6 +72,61 @@ public class WeekCalendarFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_weekcalendar, container, false);
+        View rootView = inflater.inflate(R.layout.fragment_weekcalendar, container, false);
+
+        cal = Calendar.getInstance();
+
+        GridView gridview = (GridView)rootView.findViewById(R.id.week_gridview);
+
+        takeWeekCalendar();
+        getBlank();
+
+        WeekCalendarAdapter WeekCal;
+
+        //가로모드일 때
+        if(getActivity().getWindowManager().getDefaultDisplay().getRotation()
+                == Surface.ROTATION_90||getActivity().getWindowManager().getDefaultDisplay().getRotation()== Surface.ROTATION_270){
+            WeekCal = new WeekCalendarAdapter(getActivity(),android.R.layout.simple_list_item_1, days,130);
+        }else{  //세로모드일 때
+            WeekCal = new WeekCalendarAdapter(getActivity(),android.R.layout.simple_list_item_1, days,250);
+        }
+        gridview.setAdapter(WeekCal);
+
+        gridview.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int position, long id) {
+                String date = WeekCal.getItem(position);
+                Toast.makeText(getActivity(), "position = "+date,Toast.LENGTH_SHORT).show();
+                // *************** 토스트 메시지 date 부분 수정 ***************
+                // 선택된 격자의 position을 나타내야함. (날짜랑은 다름)
+            }
+        });
+
+        return rootView;
     }
+
+    private void takeWeekCalendar(){ // *************** 이 함수는 요일 밑에 한 줄을 나타냄, 일주일씩만 출력함
+        // *************** 주간 출력 함수 다시 만들어야 함 ***************
+        cal.set(year,month,1);
+        int start_day = cal.get(Calendar.DAY_OF_WEEK); //첫 날
+        int finish_day = cal.getActualMaximum(Calendar.DATE); //마지막 날
+        int i;
+
+        if(start_day != 1) {
+            for(i=0; i<start_day-1; i++) {
+                days.add(" ");
+            }
+        }
+        for (i=1; i<=finish_day; i++) {
+            days.add(Integer.toString(i));
+        }
+        for (i=1; i<42-(start_day+finish_day-1)+1; i++) {
+            days.add(" ");
+        }
+    }
+
+    private void getBlank() { // *************** 이 함수는 주간 격자 출력 함수
+
+    }
+
 }
