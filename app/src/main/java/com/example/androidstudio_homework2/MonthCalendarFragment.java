97c51f823bb3441f703579e76e7a6b9e327a9ffb
Sebbange <sebbang99@gmail.com>
2022-05-02 16:06:28 +0900

    빈칸 오류
    수정중

--- app/src/main/java/com/example/androidstudio_homework2/MonthCalendarFragment.java
+++ app/src/main/java/com/example/androidstudio_homework2/MonthCalendarFragment.java
@@ -114,5 +114,8 @@ public class MonthCalendarFragment extends Fragment {
             days.add(Integer.toString(i)); //형변환
         } // 매월 1일이 요일과 일치하면 (ex: 22년 4월 1일은 금요일) 그때부터 해당 말일(4월은 30일)까지 날짜 출력
         //날짜는 1일부터 마지막까지
+        for (i=0; i<42-days.size(); i++) {
+            days.add(" "); //달력 나머지 부분 흰색 그리드뷰로 채우기
+        }
     }
 }
