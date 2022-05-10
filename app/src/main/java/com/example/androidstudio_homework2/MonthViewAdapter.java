85e933592f378796a37538f004602eb7a5adfbd7
Sebbange <sebbang99@gmail.com>
2022-05-10 20:07:41 +0900

    (방민족 수정) 월간/주간 색변화
    월간 달력과 주간 달력 선택시 색이 변화하는 코드를 수정했습니다

--- app/src/main/java/com/example/androidstudio_homework2/MonthViewAdapter.java
+++ app/src/main/java/com/example/androidstudio_homework2/MonthViewAdapter.java
@@ -28,18 +28,18 @@ public class MonthViewAdapter extends FragmentStateAdapter {
         //계산 수정할거임!!!!! 왜 딜레이가 걸릴까요?
         //https://furang-note.tistory.com/29
         position = position-(NUM_ITEMS/2); //캘린더 위치 식별을 편하게 하기 위해
-        int s_p = (position+month)%12;
-        int sp = (position+month)/12;
+        int sp_m = (position+month)%12; //month (0~11 or -1~-12)
+        int sp_y = (position+month)/12; //year (...-2 -> -1 -> 0 (현재) -> 1 -> 2...)
         int p_year;
         int p_month;
         //스와이프된 년과 월
-        if(s_p<0) { //왼쪽으로 스와이프해서 년도가 바뀌었을 때
-            p_year = year+sp-1;
-            p_month = 12+s_p;
+        if(sp_m<0) { //왼쪽으로 스와이프해서 년도가 바뀌었을 때
+            p_year = year+sp_y-1;
+            p_month = 12+sp_m;
         }
         else { //오른쪽으로 스와이프 or 왼쪽으로 스와이프 했는데 년도는 그대로 일 때(현재 1월이 아닌 경우)
-            p_year = year+sp;
-            p_month = s_p;
+            p_year = year+sp_y;
+            p_month = sp_m;
         }
         //MonthCalendarFragment의 Instance메소드를 통해 스와이프 한 year과 month 반환
         return MonthCalendarFragment.newInstance(p_year, p_month);
